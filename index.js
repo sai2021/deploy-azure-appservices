4f7b0bd9ac412435a5e2d134b749530332dad2d7
saikumar <saikumar@cawstudios.com>
2020-04-14 13:20:25 +0530

    Change main code

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("Hello World for the testing the appservice with node!");
 });
 
 const port = process.env.PORT || 1337;
